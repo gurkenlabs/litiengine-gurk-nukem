fb7a9aa523af3345c537ca16b41fc60fce5f6d8b
Steffen Wilke <mail@steffen-wilke.de>
2018-12-22 00:35:50 +0100

    Upgrade gradle to 5.0. While developing, we'll directly reference the litiengine.

--- src/com/litiengine/gurknukem/Program.java
+++ src/com/litiengine/gurknukem/Program.java
@@ -3,8 +3,8 @@ package com.litiengine.gurknukem;
 import java.awt.event.KeyEvent;
 
 import de.gurkenlabs.litiengine.Game;
-import de.gurkenlabs.litiengine.Resources;
 import de.gurkenlabs.litiengine.input.Input;
+import de.gurkenlabs.litiengine.resources.Resources;
 
 /**
  * This class provides the main entry point for the LITIengine game "GURK NUKEM".
@@ -19,17 +19,17 @@ public class Program {
    */
   public static void main(String[] args) {
     // set meta information about the game
-    Game.getInfo().setName("GURK NUKEM");
-    Game.getInfo().setSubTitle("");
-    Game.getInfo().setVersion("v0.0.1");
-    Game.getInfo().setWebsite("https://github.com/gurkenlabs/litiengine-gurk-nukem");
-    Game.getInfo().setDescription("An example 2D platformer with shooter elements made in the LITIengine");
+    Game.info().setName("GURK NUKEM");
+    Game.info().setSubTitle("");
+    Game.info().setVersion("v0.0.1");
+    Game.info().setWebsite("https://github.com/gurkenlabs/litiengine-gurk-nukem");
+    Game.info().setDescription("An example 2D platformer with shooter elements made in the LITIengine");
 
     // init the game infrastructure
     Game.init(args);
 
     // set the icon for the game (this has to be done after initialization because the ScreenManager will not be present otherwise)
-    Game.getScreenManager().setIconImage(Resources.getImage("icon.png"));
+    Game.window().setIconImage(Resources.images().get("icon.png"));
 
     // make the game exit upon pressing ESCAPE (by default there is no such key binding and the window needs to be shutdown otherwise, e.g. ALT-F4 on Windows)
     Input.keyboard().onKeyPressed(KeyEvent.VK_ESCAPE, e -> System.exit(0));
