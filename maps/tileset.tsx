4070673bcb0601b1b747c9cc5478582a53208d30
Matthias Wilke <matthias@gurkenlabs.de>
2018-12-22 14:51:55 +0100

    Add level 1

---
+++ maps/tileset.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.1" name="tileset" tilewidth="8" tileheight="8" tilecount="256" columns="16">
+ <image source="tileset.png" width="128" height="128"/>
+ <tile id="7">
+  <animation>
+   <frame tileid="7" duration="300"/>
+   <frame tileid="8" duration="100"/>
+  </animation>
+ </tile>
+</tileset>
