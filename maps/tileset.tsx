f8bc9d073ab3f5f7097e1ee787ebec2bdf50ee2f
Matthias Wilke <matthias@gurkenlabs.de>
2018-12-24 00:21:48 +0100

    rework clouds in the tileset Add music track.

--- maps/tileset.tsx
+++ maps/tileset.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.2.1" name="tileset" tilewidth="8" tileheight="8" tilecount="256" columns="16">
+<tileset firstgid="0" name="tileset" tilewidth="8" tileheight="8" tilecount="256" columns="16">
  <image source="tileset.png" width="128" height="128"/>
  <tile id="7">
   <animation>
