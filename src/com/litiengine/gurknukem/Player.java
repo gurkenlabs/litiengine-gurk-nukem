6d76739bc0dcde70d95ef20816dd9761b702dadd
Steffen Wilke <mail@steffen-wilke.de>
2019-01-12 16:55:05 +0100

    Make use of the new Entity Action mechanism.

--- src/com/litiengine/gurknukem/Player.java
+++ src/com/litiengine/gurknukem/Player.java
@@ -7,17 +7,17 @@ import com.litiengine.gurknukem.abilities.Jump;
 import de.gurkenlabs.litiengine.Game;
 import de.gurkenlabs.litiengine.IUpdateable;
 import de.gurkenlabs.litiengine.annotation.CollisionInfo;
+import de.gurkenlabs.litiengine.annotation.Action;
 import de.gurkenlabs.litiengine.annotation.EntityInfo;
 import de.gurkenlabs.litiengine.annotation.MovementInfo;
 import de.gurkenlabs.litiengine.entities.Creature;
-import de.gurkenlabs.litiengine.entities.IJumpingEntity;
 import de.gurkenlabs.litiengine.input.PlatformingMovementController;
 import de.gurkenlabs.litiengine.physics.CollisionType;
 
 @EntityInfo(width = 18, height = 18)
 @MovementInfo(velocity = 70)
 @CollisionInfo(collisionBoxWidth = 8, collisionBoxHeight = 16, collision = true)
-public class Player extends Creature implements IUpdateable, IJumpingEntity {
+public class Player extends Creature implements IUpdateable {
   public static final int MAX_ADDITIONAL_JUMPS = 1;
 
   private static Player instance;
@@ -52,7 +52,15 @@ public class Player extends Creature implements IUpdateable, IJumpingEntity {
     }
   }
 
-  @Override
+  /**
+   * Checks whether this instance can currently jump and then performs the <code>Jump</code> ability.
+   * <p>
+   * <i>Note that the name of this methods needs to be equal to {@link PlatformingMovementController#JUMP}} in order for the controller to be able to
+   * use this method. <br>
+   * Another option is to explicitly specify the <code>Action.name()</code> attribute on the annotation.</i>
+   * </p>
+   */
+  @Action(description = "This performs the jump ability for the player's entity.")
   public void jump() {
     if (this.consecutiveJumps >= MAX_ADDITIONAL_JUMPS || !this.jump.canCast()) {
       return;
